f43b94db3f297462c87757152b4979817a666d56
gopikrishnnamaringanti <gopikrishnna36@gmail.com>
2024-06-14 22:36:59 +0530

    commit

---
+++ py-myopl-code/Program/run_basic.bat
@@ -0,0 +1,3 @@
+@echo off
+python -c "import sys; sys.path.append(r'C:\Users\M Gopi krishnna\Desktop\Programming language\py-myopl-code\Program'); from shell import run_basic_shell; run_basic_shell()"
+pause
